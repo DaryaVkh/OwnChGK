735c643477f23e475f10fbb78adeb959da339082
Shrein Olga <olja_sr@mail.ru>
2021-11-16 22:03:10 +0500

    websocket

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,17 @@
 import server from './server';
+import * as WebSocket from 'ws';
 
 const port = parseInt(process.env.PORT || '3000');
+
+const wss = new WebSocket.Server({ port: 80 });
+
+wss.on('connection', (ws: WebSocket) => {
+    ws.on('message', (message: string) => {
+        console.log('received: %s', message);
+        ws.send(`Hello, you sent -> ${message}`);
+    });
+    ws.send('Hi there, I am a WebSocket server');
+});
 
 const starter = new server().start(port)
     .then(port => console.log(`Running on port ${port}`))
