63adaf56a0d114c92c73df9f175424b999979779
prefie <prefie@bk.ru>
2022-01-05 19:26:48 +0500

    wss

--- frontend/src/commonFunctions.ts
+++ frontend/src/commonFunctions.ts
@@ -6,5 +6,5 @@ export const getCookie = (name: string) => {
 }
 
 export const getUrlForSocket = () => {
-    return `ws://${window.location.host.split(':')[0]}:1025`;
+    return `wss://${window.location.host.split(':')[0]}:1025`;
 }
