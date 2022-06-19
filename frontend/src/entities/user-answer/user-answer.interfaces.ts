f6be3b37dfd6ef8376f955c3af4956ac34d876f5
prefie <prefie@bk.ru>
2022-06-19 22:50:17 +0500

    fix merge

--- frontend/src/entities/user-answer/user-answer.interfaces.ts
+++ frontend/src/entities/user-answer/user-answer.interfaces.ts
@@ -1,6 +1,5 @@
 export interface UserAnswerProps {
     answer?: string;
-    gamePart: string;
     status: 'success' | 'error' | 'opposition' | 'no-answer';
     order: number;
     gamePart: 'chgk' | 'matrix';
